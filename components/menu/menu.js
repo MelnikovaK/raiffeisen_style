b59d8a8535469369277e7618c6d20d8ab8585d6f
germanAI_work <denisskinrasskaz@ya.ru>
2019-03-06 17:30:15 +0300

    добавлена возможость выбора активного пункта меню

---
+++ components/menu/menu.js
@@ -0,0 +1,16 @@
+$(function(){
+  var $element = $('.menu');
+  var $menu_items = $('.menu__list');
+  var menu_item = '.menu__item';
+
+  //выделение выбранного элемента меню
+  $menu_items.on('click', menu_item, function() {
+    if ($(this).hasClass('menu__item_line')) return false;
+
+    $('.menu__item').each(function(i, e) {
+      $(e).removeClass('menu__item_active');
+    });
+
+    $(this).addClass('menu__item_active');
+  });
+})
